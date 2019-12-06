c1c3daade418b62753ede0a332558cd46d993047
Sorchascome <sorcishere@gmail.com>
2019-12-06 11:13:57 +0200

    Update

--- src/components/TopSection/VideoPlayer/VideoPlayer.js
+++ src/components/TopSection/VideoPlayer/VideoPlayer.js
@@ -7,7 +7,7 @@ export default class VideoPlayer extends Component {
   render () {
     return (
       <div className="VideoPlayer">
-        <ReactPlayer onClick={this.props.trackVideoPlay} url={this.props.link} playing={false} controls={true} width='100%' height='100%'/>
+        <ReactPlayer onStart={this.props.trackVideoPlay} url={this.props.link} playing={false} controls={true} width='100%' height='100%'/>
       </div>
     )
   }
